3f710a0b485916bbdf1a55e1a59cb909e65f63ce
Charlotte Koch <dressupgeekout@gmail.com>
2021-02-21 17:11:44 -0800

    Explicitly provide bindings around Vector2,3,4 and Matrix structs
    This allows me to use Raylib-cs on macOS without Visual Studio (and also
    with Mono, if that makes any difference). These definitions match what's in
    Raylib's C header file. Not sure why they're not listed here to begin with.
    Perhaps it's assumed these types would come from your system's OpenGL
    headers instead?

--- Raylib-cs/Raylib.cs
+++ Raylib-cs/Raylib.cs
@@ -5,6 +5,51 @@ using System.Security;
 
 namespace Raylib_cs
 {
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+    public struct Vector2
+    {
+      public float x;
+      public float y;
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+    public struct Vector3
+    {
+      public float x;
+      public float y;
+      public float z;
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+    public struct Vector4
+    {
+      public float x;
+      public float y;
+      public float z;
+      public float w;
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+    public struct Matrix4x4
+    {
+      public float m0;
+      public float m1;
+      public float m2;
+      public float m3;
+      public float m4;
+      public float m5;
+      public float m6;
+      public float m7;
+      public float m8;
+      public float m9;
+      public float m10;
+      public float m11;
+      public float m12;
+      public float m13;
+      public float m14;
+      public float m15;
+    }
+
     // Color type, RGBA (32bit)
     [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
     public struct Color
